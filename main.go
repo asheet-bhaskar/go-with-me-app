6af0d448dd6ddaf8813a2c37045ee6a19f507b8e
Asheet Kumar <asheet.kumar@go-jek.com>
2017-07-16 19:33:01 +0700

    adds cli command to start the API server

--- main.go
+++ main.go
@@ -6,8 +6,9 @@ import (
 
 	"github.com/heroku/go-with-me-app/appcontext"
 	"github.com/heroku/go-with-me-app/config"
-	"github.com/heroku/go-with-me-app/console"
+	"github.com/heroku/go-with-me-app/database"
 	"github.com/heroku/go-with-me-app/logger"
+	"github.com/heroku/go-with-me-app/server"
 	"github.com/urfave/cli"
 )
 
@@ -29,10 +30,18 @@ func main() {
 	clientApp.Version = "0.0.1"
 	clientApp.Commands = []cli.Command{
 		{
+			Name:        "start",
+			Description: "Starts api server",
+			Action: func(c *cli.Context) error {
+				server.StartAPIServer()
+				return nil
+			},
+		},
+		{
 			Name:        "migrate",
 			Description: "Runs database migrations",
 			Action: func(c *cli.Context) error {
-				return console.RunDatabaseMigrations()
+				return database.RunDatabaseMigrations()
 			},
 		},
 	}
