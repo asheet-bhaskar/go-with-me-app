64ada06d52982af03d7e17b9e90bdcf413bfe593
Asheet Kumar <asheet.kumar@go-jek.com>
2017-07-17 02:58:26 +0700

    adds method to set the booking status to driver not found in repository layer

--- repository/booking.go
+++ repository/booking.go
@@ -15,8 +15,9 @@ type BookingRepository struct {
 }
 
 const (
-	createBookingQuery    = `INSERT INTO bookings (booking_id, customer_id, driver_id, pick_up, destination, fare, status, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);`
-	getBookingStatusQuery = `SELECT status FROM bookings WHERE booking_id=$1;`
+	createBookingQuery                  = `INSERT INTO bookings (booking_id, customer_id, driver_id, pick_up, destination, fare, status, created_at, updated_at) VALUES($1, $2, $3, $4, $5, $6, $7, $8, $9);`
+	getBookingStatusQuery               = `SELECT status FROM bookings WHERE booking_id=$1;`
+	setBookingStatusDriverNotFoundQuery = `UPDATE bookings SET status='driver_not_found' WHERE booking_id=$1;`
 )
 
 func (br *BookingRepository) CreateBooking(booking *domain.Booking) error {
@@ -45,6 +46,23 @@ func (br *BookingRepository) GetBookingStatus(bookingId string) (string, error) 
 		return status, errors.New("Booking not found in the database")
 	}
 	return status, nil
+}
+
+func (br *BookingRepository) SetBookingStatusDriverNotFound(bookingId string) error {
+	status, err := br.GetBookingStatus(bookingId)
+	if err != nil {
+		return err
+	}
+
+	if status != "created" {
+		return errors.New("status can't be updated from " + status + "to driver not found")
+	}
+
+	_, err = br.db.Exec(setBookingStatusDriverNotFoundQuery, bookingId)
+	if err != nil {
+		return errors.New("failed to update the booking status to driver not found.")
+	}
+	return nil
 }
 
 func NewBookingRepository() *BookingRepository {
--- repository/booking_test.go
+++ repository/booking_test.go
@@ -74,6 +74,60 @@ func TestGetStatusReturnsErrorWhenBookingPresentInDatabase(t *testing.T) {
 	})
 }
 
+func TestSetBookingStatusDriverNotFoundReturnsErrorWhenBookingNotPresentInDatabase(t *testing.T) {
+	config.Load()
+	logger.SetupLogger()
+	appcontext.Initiate()
+	withCleanDB(func() {
+		bookingId := "111"
+		err := NewBookingRepository().SetBookingStatusDriverNotFound(bookingId)
+		assert.NotNil(t, err)
+	})
+}
+
+/* TODO;; add update status method
+func TestSetBookingStatusDriverNotFoundReturnsErrorWhenBookingStatusIsNotCreated(t *testing.T) {
+	config.Load()
+	logger.SetupLogger()
+	appcontext.Initiate()
+	withCleanDB(func() {
+		booking := createBooking(map[string]interface{}{
+			"CustomerID":  "111",
+			"Status":      "completed",
+			"PickUp":      "100, 100",
+			"Destination": "111, 111",
+			"Fare":        "10000.0"})
+		err := NewBookingRepository().CreateBooking(booking)
+		assert.Nil(t, err)
+
+		err = NewBookingRepository().SetBookingStatusDriverNotFound(booking.BookingID)
+		assert.NotNil(t, err)
+	})
+}
+*/
+func TestSetBookingStatusDriverNotFoundReturnsNilWhenBookingStatusIsCreated(t *testing.T) {
+	config.Load()
+	logger.SetupLogger()
+	appcontext.Initiate()
+	withCleanDB(func() {
+		booking := createBooking(map[string]interface{}{
+			"CustomerID":  "111",
+			"PickUp":      "100, 100",
+			"Destination": "111, 111",
+			"Fare":        "10000.0"})
+		err := NewBookingRepository().CreateBooking(booking)
+		assert.Nil(t, err)
+
+		err = NewBookingRepository().SetBookingStatusDriverNotFound(booking.BookingID)
+		assert.Nil(t, err)
+
+		expectedStatus := "driver_not_found"
+		observedStatus, err := NewBookingRepository().GetBookingStatus(booking.BookingID)
+		assert.Nil(t, err)
+		assert.Equal(t, expectedStatus, observedStatus)
+	})
+}
+
 func createBooking(data map[string]interface{}) *domain.Booking {
 	return factories.BookingFactory.MustCreateWithOption(data).(*domain.Booking)
 }
