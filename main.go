858af5569db8106f901c574bdfe122a4d1e48597
Asheet Kumar <asheet.kumar@go-jek.com>
2017-07-16 19:03:11 +0700

    adds application main file; adds command to run database migrations

--- main.go
+++ main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
-	"fmt"
+	_ "expvar"
+	"os"
+
+	"github.com/heroku/go-with-me-app/appcontext"
+	"github.com/heroku/go-with-me-app/config"
+	"github.com/heroku/go-with-me-app/console"
+	"github.com/heroku/go-with-me-app/logger"
+	"github.com/urfave/cli"
 )
 
+func handleInitError() {
+	if e := recover(); e != nil {
+		logger.Log.Fatalf("Failed to load the app due to error : %s", e)
+	}
+}
+
 func main() {
-	fmt.Println("Hello World!")
+	defer handleInitError()
+
+	config.Load()
+	logger.SetupLogger()
+	appcontext.Initiate()
+
+	clientApp := cli.NewApp()
+	clientApp.Name = "go-with-me-app"
+	clientApp.Version = "0.0.1"
+	clientApp.Commands = []cli.Command{
+		{
+			Name:        "migrate",
+			Description: "Runs database migrations",
+			Action: func(c *cli.Context) error {
+				return console.RunDatabaseMigrations()
+			},
+		},
+	}
+
+	if err := clientApp.Run(os.Args); err != nil {
+		panic(err)
+	}
 }
