e18759eaa52e2448993b9928c044ba96d56f622f
Asheet Kumar <asheet.kumar@go-jek.com>
2017-07-17 02:01:24 +0700

    adds path in router to get the booking status

--- server/router.go
+++ server/router.go
@@ -12,6 +12,7 @@ func Router(services *service.Services) http.Handler {
 	router := mux.NewRouter()
 	router.HandleFunc("/ping", handler.PingHandler).Methods("GET")
 	router.HandleFunc("/v1/booking/create", handler.CreateBookingHandler(services)).Methods("POST")
+	router.HandleFunc("/v1/booking/status", handler.GetBookingStatusHandler(services)).Methods("GET")
 	router.NotFoundHandler = http.HandlerFunc(handler.NotFoundHandler)
 	return router
 }
