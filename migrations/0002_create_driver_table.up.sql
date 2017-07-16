8a842f9cbe875ba0f79de0d13f338fad9b054b42
Asheet Kumar <asheet.kumar@go-jek.com>
2017-07-16 19:00:04 +0700

    adds database migration to create driver table

---
+++ migrations/0002_create_driver_table.up.sql
@@ -0,0 +1,9 @@
+CREATE TABLE drivers(
+	id bigserial primary key,
+	driver_id varchar not null,
+	name varchar not null,
+	email varchar not null,
+	status varchar not null,
+	created_at timestamp without time zone not null, 
+	updated_at timestamp without time zone not null 
+);
